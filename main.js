72f2bab4219f3d5fbffa7ec4420950d19917f719
cxoltero <carlosh95@gmail.com>
2014-10-29 22:39:44 -0400

    began the js part of the project, testing events on cells 11, 21, 12, 22, at this point they are all working fine

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-alert("js is working!");
+$(document).ready(function(){
+
+  $("#cell11 , #cell12, #cell21, #cell22")
+    .click(function(){
+      alert("click");
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
