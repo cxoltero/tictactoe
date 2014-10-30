2df3520cc4f2301dd7549c93e184186dbe6fdac2
cxoltero <carlosh95@gmail.com>
2014-10-29 23:35:20 -0400

    fixed the url of the jquery, also tryed adding the css atributes to implemente the Os and Xs, but as of now, it does not work

--- main.js
+++ main.js
@@ -1,22 +1,24 @@
 $(document).ready(function(){
+  var turn = 0;
 
-  $("#cell11 , #cell12, #cell13, #cell21, #cell22, #cell23, #cell31, #cell32, #cell33")
-    .click(function(){
-      alert("click");
-    });
+  $("#cell11 , #cell12, #cell13, #cell21, #cell22, #cell23, #cell31, #cell32, #cell33 ")
+    .click(
+      function(){
+        // alert("click");
+        var cell = $(this);
+          if(turn=== 0){
+            turn = 1 ;
+            cell.css("background", "url(images/o.png");
+
+          }else{
+            turn = 0;
+            cell.css("background", "url(images/o.png");
+          }
+      }
+    );
+
+});
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-})
