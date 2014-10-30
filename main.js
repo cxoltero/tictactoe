94a31d16a4623ca2aa6508b2e90f63df9a23d96e
cxoltero <carlosh95@gmail.com>
2014-10-30 08:51:14 -0400

    now it is working, there was a type with the parentesis, now off to launch to firebase

--- main.js
+++ main.js
@@ -8,15 +8,15 @@ $(document).ready(function(){
         var cell = $(this);
           if(turn=== 0){
 
-            cell.css("background", "url(images/o.png");
+            cell.css("background", "url(images/o.png)");
             turn = 1 ;
 
           }else{
-            cell.css("background", "url(images/o.png");
+            cell.css("background", "url(images/x.png)");
             turn = 0;
           }
       }
-    );
+      );
 
 });
 
